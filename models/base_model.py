d4a188c7d0483fd22fa41a972d89257748e1d5a8
Diab <odimayodavid7@gmail.com>
2022-10-31 16:24:43 +0000

    Updated base model file

--- models/base_model.py
+++ models/base_model.py
@@ -1,66 +1,38 @@
 # #!/usr/bin/env python3
-
 """
-A model that implements base model class
+A module that implements the base model class
 """
 from datetime import datetime
-import imp
-
-import uuid
+import time
+from uuid import uuid4
 
 
 class BaseModel:
-	"""
-	A class that deines all common attributes/methods for other classes
-	"""
-	def __init__(self, *args, **kwargs):
-		"""
-		A fuction that defines the instance attributes
+    """
+    A class that defines all common attributes/methods for other classes
+    """
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-		"""
-		from models import storage
-		if not kwargs:
-			self.id = str(uuid.uuid4())
-			self.created_at = datetime.now()
-			self.updated_at = datetime.now()
-			storage.new(self)
-		else:
-			for key, value in kwargs.items():
-				if key != '__class__':
-					if key in ('created_at', 'updated_at'):
-						setattr(self, key, datetime.fromisoformat(value))
-					else:
-						setattr(self, key, value)
-	
-	
-	def __str__(self):
-		"""
-        Returns the string representation of BaseModel object.
-        [<class name>] (<self.id>) <self.__dict__>
-        """
-		return "[{}] ({}) <{}>".format(type(self).__name__, self.id, self.__dict__)
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>"
 
-	def save(self):
-		"""
-        Updates 'self.updated_at' with the current datetime
-        """
-		from models import storage
-		self.updated_at = datetime.now()
-		storage.save()
-	
-	def to_dict(self):
-		"""
-        returns a dictionary containing all keys/values of __dict__
-        of the instance:
-        - only instance attributes set will be returned
-        - a key __class__ is added with the class name of the object
-        - created_at and updated_at must be converted to string object in ISO
-        object
-        """
-		newdict = self.__dict__.copy()
-		newdict["__class__"] = self.__class__.__name__
-		for key, value in self.__dict__.items():
-			if key in ("created_at", "updated_at"):
-				value = self.__dict__[key].isoformat()
-				newdict[key] = value
-		return newdict
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        newdict = self.__dict__.copy()
+        newdict["__class__"] = self.__class__.__name__
+        for key, value in newdict.items():
+            if key in ("created_at", "updated_at"):
+                value = newdict[key].isoformat()
+                newdict[key] = value
+        return newdict
+
+
+b = BaseModel()
+print(b.to_dict())
+print("==============")
+print(b.__str__())
