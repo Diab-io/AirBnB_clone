0480a99712c1a071b9d798b44fd04a7b65387146
Diab <odimayodavid7@gmail.com>
2022-11-01 13:34:30 +0000

    created console

---
+++ console.py
@@ -0,0 +1,26 @@
+# #!/usr/bin/python3
+""" Defines the hbnh console """
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Defines the HolbertonBnB command interpreter.
+    Attributes:
+        prompt (str): The command prompt.
+    """
+    intro = " Welcome to my AirBnB "
+    prompt = " (hbnh) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        print("")
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
